c3127bc15e6af4c78b8b19cdd66416bcaf514495
Adedapo Aderemi <adedapopaul@yahoo.com>
2016-10-16 19:52:12 +0100

    The first test has been tested and passed

--- javascript-tdd-setup/tests.js
+++ javascript-tdd-setup/tests.js
@@ -1,11 +1,13 @@
 'use strict'
 
+var chai = require('chai');
+var assert = chai.assert;
+
 var lib = require('./lib/sum_of_primes.js');
 
 describe("Test that constants are computed properly", function() {
   it("should give 10, when 5 is passed", function() {
-    assert(lib.sum_of_primes(5))===10;
+    assert(lib.sum_of_primes(5)==10);
   });
  
   });
-  
